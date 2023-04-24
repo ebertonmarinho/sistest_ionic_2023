030a29dac163dd8ee468ec99c1a0ad61adf94de3
ebertonmarinho <eberton.marinho@escolar.ifrn.edu.br>
2023-04-24 14:40:28 -0300

    versao 0.3

--- src/app/add-usuario/add-usuario.page.ts
+++ src/app/add-usuario/add-usuario.page.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-usuario.page.scss'],
 })
 export class AddUsuarioPage implements OnInit {
+  
+  nome: string = "";
+  idusuario: string = "";
+  cpf : string = "";
+  num_CNH : string = "";
+  validade_CNH : string = "";
+  telefone : string = "";
+  email : string = "";
 
   constructor() { }
 
